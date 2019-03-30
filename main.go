7a93cf393a7af85a995b5899d4df48cbb1a68ece
Jens Devloo <jens.devloo@pwc.com>
2019-03-30 13:49:43 +0100

    feat: add basic webserver

--- main.go
+++ main.go
@@ -3,24 +3,45 @@ package main
 import (
 	"fmt"
 	"time"
+ 	"net/http"
+ 	"strings"
 
 	"github.com/stianeikeland/go-rpio"
 )
 
-func main() {
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message
+	w.Write([]byte(message))
+
 	fmt.Println("opening gpio")
+	
+	toggleLeds()
+
+}
+
+func toggleLeds(){
 	err := rpio.Open()
 	if err != nil {
 		panic(fmt.Sprint("unable to open gpio", err.Error()))
 	}
 
-	defer rpio.Close()
-
 	pin := rpio.Pin(18)
 	pin.Output()
+
+	defer rpio.Close()
 
 	for x := 0; x < 20; x++ {
 		pin.Toggle()
 		time.Sleep(time.Second / 5)
 	}
+}
+
+func main() {
+	http.HandleFunc("/", sayHello)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+	toggleLeds()
 }
